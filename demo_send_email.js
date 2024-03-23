64bb33f323d2a6a41c04ddfca72081e638ae65f2
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-03-23 02:58:24 -0700

    Node JS Updating 3rd time

---
+++ demo_send_email.js
@@ -0,0 +1,52 @@
+// var nodemailer = require('nodemailer');
+
+// var transpoter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'usamaisrar1190@gmail.com',
+//         pass: 'uSam@1190'
+//     }
+// });
+
+// var mailOptions = {
+//     from: 'usamaisrar1190@gmail.com',
+//     to: 'osamaisrar1191@gmail.com',
+//     subject: 'Sending Email using Node.js',
+//     text: 'That was easy!'
+// }
+
+// transpoter.sendMail(mailOptions, function(error, info) {
+//     if(error) {
+//         console.log(error);
+//     }
+//     else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// })
+
+
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'usamaisrar1190@gmail.com',
+    pass: 'uSam@1190'
+  }
+});
+
+var mailOptions = {
+  from: 'usamaisrar1190@gmail.com',
+  to: 'osamaisrar1191@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
--- demo_uploadfile.js
+++ demo_uploadfile.js
@@ -1,11 +1,56 @@
-var formidable = require('formidable');
+// var formidable = require('formidable');
+// var http = require('http');
+// var fs = require('fs');
+
+// http.createServer(function(req, res) {
+//     if(req.url == '/fileupload') {
+//         var form = new formidable.IncomingForm();
+//         form.parse(req, function(err, fields, files) {
+            
+//             var oldPath = files.filetoupload.filepath;
+//             var newPath = 'G:\W3schools\Node JS\Node-JS/' + files.filetoupload.originalFilename;
+//             fs.rename(oldPath, newPath, function(err) {
+//                 if(err) throw err;
+//             })
+
+//             res.write('File uploaded and moved');
+//             res.end();
+//         });
+//     }
+//     else {
+//         res.writeHead(200, {'Content-Type':'text/html'});
+
+//         res.write('<form action="fileupload" method="post" enctype="multipart/formdata">');
+//         res.write('<input type="file" name="fileupload"><br>');
+//         res.write('<input type="submit">');
+//         res.end();
+//     }
+// }).listen(8080);
+
+
+
 var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type':'text/html'});
-
-    res.write('<form action="fileupload" method="post" enctype="multipart/formdata">');
-    res.write('<input type="file" name="fileupload"><br>');
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = 'G:\W3schools\Node JS\Node-JS' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
     res.write('<input type="submit">');
-    res.end();
+    res.write('</form>');
+    return res.end();
+  }
 }).listen(8080);
