af972d9c9dc3ac4925be110904d1136146d82ddf
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-03-26 03:16:34 -0700

    Updated MySql Learning

--- demo_send_email.js
+++ demo_send_email.js
@@ -26,27 +26,27 @@
 
 
 
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'usamaisrar1190@gmail.com',
-    pass: 'uSam@1190'
-  }
-});
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'usamaisrar1190@gmail.com',
+//     pass: 'uSam@1190'
+//   }
+// });
 
-var mailOptions = {
-  from: 'usamaisrar1190@gmail.com',
-  to: 'osamaisrar1191@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
+// var mailOptions = {
+//   from: 'usamaisrar1190@gmail.com',
+//   to: 'osamaisrar1191@gmail.com',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
